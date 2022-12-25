af087134a527f634c5078c4ca65fe763a138ceed
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-12-26 00:59:43 +0900

    style: 필드 기본값 제거

--- src/main/java/com/instaimg/crawl/MainApplication.java
+++ src/main/java/com/instaimg/crawl/MainApplication.java
@@ -2,18 +2,19 @@ package com.instaimg.crawl;
 
 import com.instaimg.crawl.controller.ImgUrlController;
 import javafx.scene.control.TextArea;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
 
 public class MainApplication {
-    public static boolean saveImages(TextArea process, String nickname, String filePath, String imgCount) throws ParseException, IOException, InterruptedException {
+    public static boolean saveImages(TextArea process, String nickname, String filePath, String imgCount) {
         ImgUrlController imgUrlController = new ImgUrlController();
         int count = Integer.MAX_VALUE;
         if (!imgCount.isBlank()) {
             count = Integer.parseInt(imgCount);
         }
         process.setText("***Nickname : " + nickname + "\n" + "***FilePath : " + filePath + "\n");
-        return imgUrlController.saveImgInLocal(nickname, filePath, count, process);
+        try {
+            return imgUrlController.saveImgInLocal(nickname, filePath, count, process);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
